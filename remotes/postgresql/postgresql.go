85cfd1eaab944a9c2b633a067401f6d2c7d06f5d
João Carlos Pandolfi <joaopandolfi@gmail.com>
2020-05-18 17:01:44 -0300

    Postgresql Pool

--- remotes/postgresql/postgresql.go
+++ remotes/postgresql/postgresql.go
@@ -10,11 +10,35 @@ type pDriver struct {
 	Case string
 }
 
-// Driver postgresqk
+// Driver postgresql
 var Driver pDriver
+
+// Pool postgresql
+var Pool map[string]pDriver
 
 // Init function used to initialize mysql database
 func Init() {
 	Driver = pDriver{Case: "lower"}
 	Driver.Init("postgres", configurations.Configuration.PostgreSQL)
+}
+
+// MakePool driver
+func MakePool(key, url string) {
+	d := pDriver{Case: "lower"}
+	d.Init("postgres", url)
+
+	if Pool == nil {
+		Pool = make(map[string]pDriver)
+	}
+	Pool[key] = d
+}
+
+// ClosePool database
+func ClosePool() {
+	if Pool == nil {
+		return
+	}
+	for _, p := range Pool {
+		p.Close()
+	}
 }
