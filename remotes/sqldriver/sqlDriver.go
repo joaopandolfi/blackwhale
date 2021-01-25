513b8dcd81f881b19e3881e1e82b1cbeebde3c2f
João Carlos Pandolfi <joaopandolfi@gmail.com>
2021-01-25 15:53:59 -0300

    query to json SQL

--- remotes/sqldriver/sqlDriver.go
+++ remotes/sqldriver/sqlDriver.go
@@ -166,3 +166,55 @@ func (cc SqlDriver) QueryToMap(theCase string, sqlStatement string, sqlParams ..
 
 	return
 }
+
+// QueryToJSON - return ditectly on byte array
+func (cc SqlDriver) QueryToJSON(sqlStatement string, sqlParams ...interface{}) ([]byte, error) {
+	cc.getDB()
+	rows, err := cc.Database.Query(sqlStatement, sqlParams...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	tableData := make([]map[string]interface{}, 0)
+
+	count := len(columns)
+	values := make([]interface{}, count)
+	scanArgs := make([]interface{}, count)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err := rows.Scan(scanArgs...)
+		if err != nil {
+			return nil, err
+		}
+
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			v := values[i]
+
+			b, ok := v.([]byte)
+			if ok {
+				entry[col] = string(b)
+			} else {
+				entry[col] = v
+			}
+		}
+
+		tableData = append(tableData, entry)
+	}
+
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
