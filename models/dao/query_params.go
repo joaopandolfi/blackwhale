581fbe176005227b567873f5c87de6bece5e19f2
jose <guislima47@gmail.com>
2022-05-17 19:52:53 -0300

    Add QueryParams struct in dao

---
+++ models/dao/query_params.go
@@ -0,0 +1,31 @@
+package dao
+
+type QueryParams struct {
+	data       map[string]interface{}
+	GenericVal string
+}
+
+func (d *QueryParams) init() {
+	if d.data == nil {
+		d.data = map[string]interface{}{}
+	}
+}
+
+func (d *QueryParams) Parse() map[string]interface{} {
+	return d.data
+}
+
+func (d *QueryParams) AddParam(column string, condition string, val interface{}) {
+	d.init()
+	d.data[fmt.Sprintf("%s:%s", column, condition)] = val
+}
+
+func (d *QueryParams) AddLike(column, val string) {
+	d.init()
+
+	d.data[fmt.Sprintf("%s:%s", likeParam, column)] = val
+}
+
+func (d *QueryParams) Data() *map[string]interface{} {
+	return &d.data
+}
