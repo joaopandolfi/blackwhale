c3991105dc07e49416a1e9932d52f3671e97f216
joaopandolfi <joaopandolfi@gmail.com>
2022-12-15 13:11:45 -0300

    fix validator

--- handlers/validators.go
+++ handlers/validators.go
@@ -1,12 +1,9 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator"
-	"github.com/joaopandolfi/blackwhale/handlers/errors"
-	"github.com/joaopandolfi/blackwhale/utils"
 )
 
 // defaultValidator its a struct validator
@@ -14,21 +11,17 @@ var defaultValidator = validator.New()
 
 // UnmarshalSnakeCaseAndValidate -
 // Unmarshal payload using snake case and validade using default validator
-func UnmarshalSnakeCaseAndValidate(w http.ResponseWriter, r *http.Request, functionName string, v interface{}) error {
+func UnmarshalSnakeCaseAndValidate(w http.ResponseWriter, r *http.Request, v interface{}) (string, error) {
 
 	err := SnakeCaseDecoder(r.Body).Decode(v)
 	if err != nil {
-		utils.Error(fmt.Sprintf("%s - unmarshaling", functionName), err.Error())
-		ResponseTypedError(w, errors.ErrorCodeInvalidBody, errors.ErrorMessageInvalidBody, err)
-		return err
+		return "unmarshaling", err
 	}
 
 	err = defaultValidator.Struct(v)
 	if err != nil {
-		utils.Error(fmt.Sprintf("%s - validating body", functionName), err.Error())
-		ResponseTypedError(w, errors.ErrorCodeInvalidBody, errors.ErrorMessageInvalidBody, err)
-		return err
+		return "validating body", err
 	}
 
-	return nil
+	return "", nil
 }
